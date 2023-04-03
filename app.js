8df258e129710b611c002a57d12d0cc9aeb61bac
Glitch (messenger-platform-quick-start) <none>
2023-04-03 03:34:56 +0000

    🍮🌒 Checkpoint ./app.js:56626904/1548

--- app.js
+++ app.js
@@ -42,31 +42,36 @@ app.use(json());
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+app.get("/success", (req, res) => {
+  res.send("Sign In Successfully");
 });
 
-// Accepts POST requests at /webhook endpoint
-app.post("/webhook", (req, res) => {
+app.post('/webhook', (req, res) => {  
+
   // Parse the request body from the POST
   let body = req.body;
 
   // Check the webhook event is from a Page subscription
-  if (body.object === "page") {
+  if (body.object === 'page') {
+
     // Iterate over each entry - there may be multiple if batched
-    body.entry.forEach(function (entry) {
-      // Get the webhook event. entry.messaging is an array, but
+    body.entry.forEach(function(entry) {
+
+      // Get the webhook event. entry.messaging is an array, but 
       // will only ever contain one event, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+      
     });
 
     // Return a '200 OK' response to all events
-    res.status(200).send("EVENT_RECEIVED");
+    res.status(200).send('EVENT_RECEIVED');
+
   } else {
     // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
+
 });
 
 // Add support for GET requests to our webhook
