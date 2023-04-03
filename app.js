e84f14aa344b498228c699caf2fdd5f9ef15f52b
Glitch (messenger-platform-quick-start) <none>
2023-04-03 07:16:03 +0000

    🙅💄 Checkpoint ./app.js:56626904/1005 ./bot-responses:56626904/1005

--- app.js
+++ app.js
@@ -156,48 +156,7 @@ app.get("/oauth2callback", async (req, res) => {
   });
 });
 
-async function askGPT(question) {
-  const apiEndpoint =
-    "https://api.openai.com/v1/engines/text-davinci-003/completions";
-  const accessToken = "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";
 
-  async function askQuestion(question) {
-    try {
-      const response = await axios.post(
-        apiEndpoint,
-        {
-          prompt: `Q: ${question}\nA:`,
-          max_tokens: 50,
-          n: 1,
-          stop: "\n",
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      return response.data.choices[0].text.trim();
-    } catch (error) {
-      console.error(`Error asking question: ${question}`, error.response.data);
-    }
-  }
-
-  const answer = await askQuestion(question).then((res) => res);
-
-  /*
-    if (answer) {
-        console.log(`Q: ${question}\nA: ${answer}`);
-    }
-    */
-  if (answer) {
-    return `A: ${answer}`;
-  } else {
-    return "Error!";
-  }
-}
 
 // Handles messages events
 async function handleMessage(sender_psid, received_message) {
