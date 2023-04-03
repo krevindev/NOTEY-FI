f5ab1cd85311fbb357851dd5a5f4bebe2a9b9144
Glitch (messenger-platform-quick-start) <none>
2023-04-03 03:45:56 +0000

    🐇💪 Checkpoint ./app.js:56626904/30767

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ const { urlencoded, json } = require("body-parser");
 
 const mongoose = require("mongoose");
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
 const mongoString =
   "mongodb+srv://batchy_bot:Tilapia-626@cluster0.kqimzoq.mongodb.net/?retryWrites=true&w=majority";
 
@@ -42,42 +45,40 @@ app.use(json());
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
 
+
+
 app.get("/success", (req, res) => {
   res.send("Sign In Successfully");
 });
 
-app.post('/webhook', (req, res) => {  
-
+app.post("/webhook", (req, res) => {
   // Parse the request body from the POST
   let body = req.body;
 
   // Check the webhook event is from a Page subscription
-  if (body.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-      // Get the webhook event. entry.messaging is an array, but 
-      // will only ever contain one event, so we get index 0
+  if (body.object === "page") {
+    body.entry.forEach(function (entry) {
+      // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
-      
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
     });
 
     // Return a '200 OK' response to all events
-    res.status(200).send('EVENT_RECEIVED');
-
+    res.status(200).send("EVENT_RECEIVED");
   } else {
     // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
-
 });
 
 // Add support for GET requests to our webhook
 
 // Accepts GET requests at the /webhook endpoint
-app.get("/webhook", (req, res) => {
+app.get("/webhoo1k", (req, res) => {
   /** UPDATE YOUR VERIFY TOKEN **/
   const VERIFY_TOKEN = "verifytoken";
 
@@ -143,3 +144,20 @@ app.get("/oauth2callback", async (req, res) => {
     res.redirect("/success");
   });
 });
+
+
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
