13a6faed94a033f7e9f9ce688af0ad4c42acacca
Glitch (messenger-platform-quick-start) <none>
2023-04-03 03:56:58 +0000

    🎭🌦 Checkpoint ./app.js:56626904/43407

--- app.js
+++ app.js
@@ -45,7 +45,9 @@ app.use(json());
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
 
-
+app.get("/", (req, res) => {
+  res.send("Running...");
+});
 
 app.get("/success", (req, res) => {
   res.send("Sign In Successfully");
@@ -57,6 +59,7 @@ app.post("/webhook", (req, res) => {
 
   // Check the webhook event is from a Page subscription
   if (body.object === "page") {
+    // Iterate over each entry - there may be multiple if batched
     body.entry.forEach(function (entry) {
       // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
@@ -65,8 +68,15 @@ app.post("/webhook", (req, res) => {
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
       console.log("Sender PSID: " + sender_psid);
-    });
 
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
+    });
     // Return a '200 OK' response to all events
     res.status(200).send("EVENT_RECEIVED");
   } else {
@@ -145,19 +155,32 @@ app.get("/oauth2callback", async (req, res) => {
   });
 });
 
-
-
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
+  let response;
 
+  // Check if the message contains text
+  if (received_message.text) {
+    // Create the payload for a basic text message
+    response = {
+      text: `You sent the message: "${received_message.text}". Now send me an image!`,
+    };
+  }
+
+  // Sends the response message
+  callSendAPI(sender_psid, response);
 }
 
 // Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-
-}
+function handlePostback(sender_psid, received_postback) {}
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-  
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
 }
