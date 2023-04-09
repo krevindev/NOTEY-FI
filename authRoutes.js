ac6202a7367d66bf4df754ce35ce6168d346e27c
Glitch (messenger-platform-quick-start) <none>
2023-04-09 07:26:46 +0000

    ⛺️🍉 Checkpoint ./authRoutes.js:56626904/1041

--- authRoutes.js
+++ authRoutes.js
@@ -8,19 +8,22 @@ const db = mongoose.connection;
 
 const { google } = require("googleapis");
 
+
 const CLIENT_ID = process.env.CLIENT_ID;
   const CLIENT_SECRET = process.env.CLIENT_SECRET;
   const REDIRECT_URI = process.env.REDIRECT_URI;
   const SCOPES = process.env.SCOPE_STRING;
 
-
-async function isSameAccount(newAccessToken, storedAccessToken) {
-  
-  const oauth2Client = new OAuth2Client(
+const oauth2Client = new OAuth2Client(
       CLIENT_ID,
       CLIENT_SECRET,
       REDIRECT_URI
     );
+
+
+
+
+async function isSameAccount(newAccessToken, storedAccessToken) {
   
   // Get the user ID for the first access token
   const res1 = await oauth2Client.tokeninfo({ access_token: newAccessToken });
@@ -45,15 +48,13 @@ authRouter.get("/oauth2callback", async (req, res) => {
   
 
   return new Promise(async (resolve, reject) => {
-    const oauth2Client = new OAuth2Client(
-      CLIENT_ID,
-      CLIENT_SECRET,
-      REDIRECT_URI
-    );
+    
     const { code } = req.query;
 
     try {
       const { tokens } = await oauth2Client.getToken(code);
+      
+      //const tokenInfo = await oauth2Client.tokeninfo({ access_token: tokens.access_token})
 
       await db.collection("noteyfi_users").updateOne(
         { psid: targetPSID },
