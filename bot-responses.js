829779cadd5a99d11197826d7d617e4ea953aa94
Glitch (messenger-platform-quick-start) <none>
2023-04-26 03:53:33 +0000

    🏢👜 Checkpoint ./bot-responses.js:56626904/181880

--- bot-responses.js
+++ bot-responses.js
@@ -770,90 +770,94 @@ async function response(msg, ...sender_psid) {
     // user cache
 
     console.log('READING CACHE')
-    const uR = await cachingFunctions.getFromCache(String(sender_psid))
-      .then(async userCache => {
-        // if the user has courses
-        if (userCache['courses']) {
-          console.log(userCache['courses'])
-          return await userCache['courses']
-        }
-      })
-      .catch(async err => {
-        // WE DO THE OLD WAYS HERE
-        const user = async () => {
-          return new Promise(async (resolve, reject) => {
-            await db
-              .collection('noteyfi_users')
-              .findOne({ psid: String(sender_psid) }, (err, result) => {
-                if (err) {
-                  reject('Rejected')
-                } else {
-                  resolve(result)
-                }
-              })
-          })
-        }
-        const token = await user()
-          .then(res => res.vle_accounts[0])
-          .catch(err => console.log(err))
+    const userCache = await cachingFunctions.getFromCache(String(sender_psid))
+    .then(res => res)
+    .catch(err => undefined);
 
-        const auth = await new google.auth.OAuth2(
-          CLIENT_ID,
-          CLIENT_SECRET,
-          REDIRECT_URI
-        )
-
-        await auth.setCredentials({
-          access_token: await token.access_token,
-          refresh_token: await token.refresh_token
-        })
-
-        const classroom = await google.classroom({
-          version: 'v1',
-          auth: auth
-        })
-
-        let courses = await classroom.courses.list({
-          courseStates: ['ACTIVE']
-        })
-
-        courses = courses.data.courses
-
-        let filteredCourses = await Promise.all(
-          courses.map(async course => {
-            const activities = await classroom.courses.courseWork.list({
-              courseId: course.id
+    
+    // if the user is in cache
+    if (await userCache) {
+      // if the user has courses
+      if (userCache['courses']) {
+        console.log(userCache['courses'])
+        return await getResponse(userCache['courses'])
+      }
+    }
+    // if the user isn't in the cache
+    else {
+      // WE DO THE OLD WAYS HERE
+      const user = async () => {
+        return new Promise(async (resolve, reject) => {
+          await db
+            .collection('noteyfi_users')
+            .findOne({ psid: String(sender_psid) }, (err, result) => {
+              if (err) {
+                reject('Rejected')
+              } else {
+                resolve(result)
+              }
             })
+        })
+      }
+      const token = await user()
+        .then(res => res.vle_accounts[0])
+        .catch(err => console.log(err))
 
-            const courseWork = (activities.data && activities.data.courseWork) || [] // Add a nullish coalescing operator to handle undefined
+      const auth = await new google.auth.OAuth2(
+        CLIENT_ID,
+        CLIENT_SECRET,
+        REDIRECT_URI
+      )
 
-            const filteredActs = courseWork
-              .map(cw => cw.dueDate)
-              .filter(c => c !== undefined)
+      await auth.setCredentials({
+        access_token: await token.access_token,
+        refresh_token: await token.refresh_token
+      })
 
-            if (filteredActs.length !== 0) {
-              return course
-            }
+      const classroom = await google.classroom({
+        version: 'v1',
+        auth: auth
+      })
+
+      let courses = await classroom.courses.list({
+        courseStates: ['ACTIVE']
+      })
+
+      courses = courses.data.courses
+
+      let filteredCourses = await Promise.all(
+        courses.map(async course => {
+          const activities = await classroom.courses.courseWork.list({
+            courseId: course.id
           })
-        )
 
-        filteredCourses = await filteredCourses.filter(
-          course => course !== undefined
-        )
+          const courseWork = (activities.data && activities.data.courseWork) || [] // Add a nullish coalescing operator to handle undefined
 
-        try {
-          await cachingFunctions.addToCache(String(sender_psid), await user().then(res => res).catch(err => console.log(err))
-            .then(async res => await cachingFunctions.updateACache(String(sender_psid), { course: filteredCourses })))
-        } catch (err) {
-          console.log(err)
-        }
+          const filteredActs = courseWork
+            .map(cw => cw.dueDate)
+            .filter(c => c !== undefined)
 
-        return await filteredCourses;
-      });
+          if (filteredActs.length !== 0) {
+            return course
+          }
+        })
+      )
 
-      return await getResponse(uR)
+      filteredCourses = await filteredCourses.filter(
+        course => course !== undefined
+      )
+
+      try {
+        await cachingFunctions.addToCache(String(sender_psid), await user().then(res => res).catch(err => console.log(err))
+          .then(async res => await cachingFunctions.updateACache(String(sender_psid), { course: filteredCourses })))
+      } catch (err) {
+        console.log(err)
+      }
+
+      return await getResponse(filteredCourses);
+    }
+
   }
-
 
   // if the message is unsubscribe then remove the user from the database
   else if (msg === 'unsubscribe') {
